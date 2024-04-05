60010ca1fcbdd22d7982c77e874513c82939deef
Rajeev Ranjan <85291867+rajeev107@users.noreply.github.com>
2024-04-05 12:28:23 +0530

    Update main.tf

--- terraform-bhs/main.tf
+++ terraform-bhs/main.tf
@@ -48,19 +48,20 @@ resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "SSH"
+  access                      = "Allow"
+  direction                   = "Inbound"
+  network_security_group_name = "azurerm_network_security_group.nsg.name"
+  priority                    = 100
+  protocol                    = "Tcp"
+  resource_group_name         = "azurerm_resource_group.rg.name"
+  destination_port_range      = 22
+  source_address_prefix       = "Internet"
+}
+
 
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
   subnet_id                 = azurerm_subnet.subnet.id
